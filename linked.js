43c651b90f1aeacb0b9ac102315e830cf0386c7c
Vikas Chandrashekar <Vikas_Chandrashekar@ryder.com>
2018-02-16 14:59:06 -0600

    Remove, value at index, index of value :newAddition

--- linked.js
+++ linked.js
@@ -22,10 +22,60 @@ class linkedList{
             currentNode.next = node;
         }
     }
+
+    remove(value){
+        let currentNode = this.head;
+        let previousNode;
+        if(currentNode.value === value){
+            this.head = currentNode.next
+        } else {
+        while(currentNode.value !== value){
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+        }
+            previousNode.next = currentNode.next;
+        }
+
+    }
+
+    indexOf(value){
+        let currentNode = this.head;
+        let index = -1;
+        while(currentNode){
+            index += 1;
+            if(currentNode.value === value){
+                return index
+            }
+            currentNode = currentNode.next;
+        }
+        return -1;
+    }
+
+    elementAt(index){
+        let i = 0;
+        let currentNode = this.head;
+        while(i < index){
+            i += 1;
+            currentNode = currentNode.next;
+        }
+        return currentNode.value;
+    }
+
+
+    
 }
 
 let li = new linkedList();
-li.add('vikas');
-li.add('mazda');
+li.add(4);
+li.add(5);
+li.add(6);
+li.add(3); 
+li.add(6);
+li.add(7);
+li.add(8);
+li.add(9);
 
-console.log(li);
+
+console.info(JSON.stringify(li));
+console.log(li.indexOf(7));
+console.log(li.elementAt(5));
